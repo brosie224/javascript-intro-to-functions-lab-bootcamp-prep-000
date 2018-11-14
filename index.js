3b48e4594731a44121c86207d294ed83ae39ca0f
Brian Rosengarten <barosengarten@gmail.com>
2018-11-14 20:02:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,16 @@ var uppercase = string.toUpperCase()
 function sayHiToGrandma(string) {
   if (string === lowercase) {
     return "I can't hear you!"
+  }
+}
+
+/* function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!"
   } else if (string === uppercase) {
     return "YES INDEED!"
   } else {
     return "I love you, too."
   }
 }
+*/
